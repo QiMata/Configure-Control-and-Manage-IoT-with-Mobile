631d98572e1ffb8e71b384a369c99856ffc3c7a0
Jared Rhodes <jrhodes@qimata.com>
2017-10-06 15:34:58 -0400

    proper moving of the files

---
+++ src/Visual Studio/Mobile/Common/ViewModels/BluetoothScanViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using QiMata.ConfigureControlManage.Annotations;
+
+namespace QiMata.ConfigureControlManage.ViewModels
+{
+    class BluetoothScanViewModel : ViewModelBase
+    {
+
+    }
+}
---
+++ src/Visual Studio/Mobile/Common/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace QiMata.ConfigureControlManage.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
